c9d346466abe60c2b105ed3353ad28024df63a1e
Anthony Dole <anthony.dole.pro@gmail.com>
2021-12-26 19:34:58 +0100

    setting heroku deploy

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ require('dotenv').config();
 const express=require('express');
 const routes = require('./routes/serverRoutes'); 
 const mongoose = require('mongoose');
+const helmet = require('helmet');
+const compression = require('compression');
 const app = express();
-
+app.use(helmet());
 app.use(express.json());
-
+app.use(compression()); //Compress all routes
 app.use('/', routes); //to use the routes
-
+app.route("/").get(function (req, res) {
+    res.sendFile(process.cwd() + "/index.html");
+  });
 const listener = app.listen(process.env.PORT || 3000, () => {
     console.log('Your app is listening on port ' + listener.address().port)
 })
@@ -17,7 +21,9 @@ const listener = app.listen(process.env.PORT || 3000, () => {
 //establish connection to database
 mongoose.connect(
    // process.env.MONGODB_URI,
-    'mongodb+srv://DiscordAdmin:Ynov2021@cluster0.law7m.mongodb.net/discordCloneAPI?retryWrites=true&w=majority',
+   // 'mongodb+srv://DiscordAdmin:Ynov2021@cluster0.law7m.mongodb.net/discordCloneAPI?retryWrites=true&w=majority',
+    process.env.MONGODB_URI,
+ 
     
     (err) => {
         if (err) return console.log("Error: ", err);
